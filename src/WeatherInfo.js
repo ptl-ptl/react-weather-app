7ee8a3d98377a711c26b2c043c799fdee36158a5
ptl-ptl <155249971+ptl-ptl@users.noreply.github.com>
2024-09-30 14:21:34 +0200

    Made a component WeatherIcon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,10 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.icon}
-              alt="Scattered Clouds"
-              className="float-left"
-            />{" "}
+            <div className="float-left">
+              {" "}
+              <WeatherIcon code={props.data.icon} />{" "}
+            </div>
             <span className="temperature">
               {Math.round(props.data.temperature)}
             </span>
